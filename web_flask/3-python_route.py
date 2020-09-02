df7aa29759434bcda8c89f51260d0af6b0164b02
derric-d <derric.donehoo@gmail.com>
2020-09-02 16:57:27 +0000

    first half

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""ez flask app"""
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def hello_holberton():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb')
+def hello_hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>')
+def get_url(text):
+    text = 'C ' + text.replace('_', ' ')
+    return text
+
+@app.route('/python/<text>/')
+@app.route('/python')
+def py_url(text='is cool'):
+    text = 'Python ' + text.replace('_', ' ')
+    return text
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', debug=True)

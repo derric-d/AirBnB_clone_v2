2d74dc2ba702f2cfc678ede9afc79d6c08b598dc
derric-d <derric.donehoo@gmail.com>
2020-09-02 17:00:31 +0000

    exe

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,8 +3,8 @@
 from flask import Flask
 
 app = Flask(__name__)
-"""app.url_map.strict_slashes = False
-"""
+app.url_map.strict_slashes = False
+
 @app.route('/')
 def hello_hbnb():
     return 'Hello HBNB!'
